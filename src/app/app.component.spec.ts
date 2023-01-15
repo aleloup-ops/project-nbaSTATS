4e15edf34a21de2e4e68eb1f6ab054532c0443e8
Alexis Leloup <alexis.leloup@epitech.eu>
2023-01-15 17:08:24 +0100

    Clean

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jenculeangular'`, () => {
+  it(`should have as title 'jangular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jenculeangular');
+    expect(app.title).toEqual('jangular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jenculeangular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('jangular app is running!');
   });
 });
